8ff7642b322c151037fed06ef5b2546777fa2506
Igor Nobre <igor.nobre@maximatech.com.br>
2020-08-08 00:27:11 -0300

    add send broadcast Teceiver

--- app/src/main/java/com/github/coutinhonobre/broadcastreceiver/CustomReceiver.kt
+++ app/src/main/java/com/github/coutinhonobre/broadcastreceiver/CustomReceiver.kt
@@ -3,11 +3,24 @@ package com.github.coutinhonobre.broadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class CustomReceiver : BroadcastReceiver() {
 
+    private val ACTION_CUSTOM_BROADCAST: String =
+        BuildConfig.APPLICATION_ID.toString() + ".ACTION_CUSTOM_BROADCAST"
+
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("CustomReceiver.onReceive() is not implemented")
+        val intentAction = intent.action
+
+        var toastMessage = "unknown intent action"
+        when (intentAction) {
+            Intent.ACTION_POWER_CONNECTED -> toastMessage = "Power connected!"
+            Intent.ACTION_POWER_DISCONNECTED -> toastMessage = "Power disconnected!"
+            ACTION_CUSTOM_BROADCAST -> toastMessage = "Custom Broadcast Received"
+        }
+
+        //Display the toast.
+        Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/github/coutinhonobre/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/github/coutinhonobre/broadcastreceiver/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.github.coutinhonobre.broadcastreceiver
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val mReceiver = CustomReceiver()
+
+    private val ACTION_CUSTOM_BROADCAST: String =
+            BuildConfig.APPLICATION_ID.toString() + ".ACTION_CUSTOM_BROADCAST"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        // Register the receiver using the activity context.
+        this.registerReceiver(mReceiver, filter);
+
+        sendBroadcastButton.setOnClickListener {
+
+            val customBroadcastIntent = Intent(ACTION_CUSTOM_BROADCAST)
+            LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
+            LocalBroadcastManager.getInstance(this)
+                    .registerReceiver(mReceiver,
+                            IntentFilter(ACTION_CUSTOM_BROADCAST));
+            //https://codelabs.developers.google.com/codelabs/android-training-broadcast-receivers/index.html?index=..%2F..android-training#3
+
+        }
+
+    }
+
+    override fun onDestroy() {
+        //Unregister the receiver
+        this.unregisterReceiver(mReceiver);
+        super.onDestroy()
     }
 }
